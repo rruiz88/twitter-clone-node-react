0083306395c143cfba933eddc5fb0dbe0ef5c1ff
rruiz88 <roman.ruiz816@gmail.com>
2023-08-01 12:22:43 -0500

    Creating react and tailwind files

---
+++ client/src/App.js
@@ -0,0 +1,53 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home/Home";
+import Profile from "./pages/Profile/Profile";
+import Explore from "./pages/Explore/Explore";
+import Signin from "./pages/Signin/Signin";
+
+const Layout = () => {
+  return (
+    <div>
+      <h1>NavBar</h1>
+      <Outlet></Outlet>
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/profile/:id",
+        element: <Profile />,
+      },
+      {
+        path: "/explore",
+        element: <Explore />,
+      },
+      {
+        path: "/signin",
+        element: <Signin />,
+      },
+      {
+        path: "/signout",
+        element: <Signin />,
+      },
+    ],
+  },
+]);
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
